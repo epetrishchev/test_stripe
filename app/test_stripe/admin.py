7e37d8b950d27a7cd5c4dbfaf66635e82c65ae7d
epetrishchev <epetrishchev@edispa.com>
2023-02-13 20:05:53 +0300

    Добавли класс отображения товаров в админке

--- app/test_stripe/admin.py
+++ app/test_stripe/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Item
 
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price']
+    list_display_links = ['name',]
+    search_fields = ['name',]
+
+
+admin.site.register(Item, ItemAdmin)
